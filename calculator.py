ecf1f8785fd07d5333ecf29e428c7ec0b916ce5a
meghnaa26 <meghnasoman26@gmail.com>
2026-05-11 09:44:31 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,36 @@
+calculate_again="yes"
+while calculate_again == "yes":
+    valid = True
+    n1 = float(input("Enter the first number: "))
+    o = input("Enter the operation want to perform(+,-,*,/,%,**,//): ")
+    n2 = float(input("Enter the second number: "))
+    result = 0
+    if o == "+":
+        result = n1 + n2
+    elif o == "-":
+        result = n1 - n2
+    elif o == "*":
+        result = n1 * n2
+    elif o == "/":
+        if n2 == 0:
+            print("Not valid")
+        else:
+            result = n1 / n2
+    elif o == "%":
+        if n2 == 0:
+            print("Not valid")
+        else:
+            result = n1 % n2
+    elif o == "//":
+        if n2 == 0:
+            print("Not valid")
+        else:
+            result = n1 // n2
+    elif o == "**":
+        result = n1 ** n2
+    else:
+        print("Enter a valid operation")
+        valid = False
+    if valid == True:
+        print("The result is",result)
+    calculate_again=input("Do you want to perform more operations: ")
