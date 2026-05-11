0686a32c0278d712bd9adcd76f4e597ae8e2ee99
meghnaa26 <meghnasoman26@gmail.com>
2026-05-11 09:42:50 +0530

    Create guessing_game.py

---
+++ guessing_game.py
@@ -0,0 +1,20 @@
+import random
+play_again = "yes"
+while play_again == "yes":
+    k = random.randint(1,100)
+    attempts = 0
+    guess = False
+    while guess == False:
+        m = int(input("Enter a number between 1-100: "))
+        attempts += 1
+        if m == k:
+            print("Correct!")
+            guess = True
+            break
+        elif m < k:
+            print("too low")
+        else:
+            print("too high")
+            
+    print("you got in",attempts,"attempts!")
+    play_again = input("Do you want to play again: ")
